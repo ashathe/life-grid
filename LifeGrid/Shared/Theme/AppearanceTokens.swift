b1264a50d9b2dc6db27952cfab4e6328ec52bfbe
ashathe <awkersey123@gmail.com>
2026-07-20 15:25:27 -0400

    feat: add responsive and accessible design foundations

---
+++ LifeGrid/Shared/Theme/AppearanceTokens.swift
@@ -0,0 +1,9 @@
+enum AppearanceToken: CaseIterable, Equatable, Sendable {
+    case background
+    case surface
+    case primaryAccent
+    case lethal
+    case destructive
+    case primaryText
+    case secondaryText
+}
