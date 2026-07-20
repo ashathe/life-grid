983347fc99754adc1f876f0ea2d024a696dc5dd0
ashathe <7953905+ashathe@users.noreply.github.com>
2026-07-20 18:30:01 -0400

    feat: add Life Grid setup controls

--- LifeGridTests/FoundationSmokeTests.swift
+++ LifeGridTests/FoundationSmokeTests.swift
@@ -1,8 +1,16 @@
+import SwiftUI
 import Testing
 @testable import LifeGrid
 
 struct FoundationSmokeTests {
     @Test func testTargetLoadsApplicationModule() {
         _ = FoundationRootView()
+    }
+
+    @Test func setupComponentsLoadApplicationModule() {
+        _ = StartingLifePicker(
+            input: .constant(StartingLifeInput(value: 40))
+        )
+        _ = Text("Card").modifier(LifeGridCard())
     }
 }
