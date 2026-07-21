83eec847ca68d872a5607a31d9d5995048475bd6
ashathe <awkersey123@gmail.com>
2026-07-21 16:32:34 -0400

    feat: add local life card

--- LifeGrid/Features/Game/ManualLifeChange.swift
+++ LifeGrid/Features/Game/ManualLifeChange.swift
@@ -1,6 +1,13 @@
+import Foundation
+
 struct ManualLifeChange: Equatable, Sendable {
     let previousValue: Int
     let currentValue: Int
+}
+
+struct LocalLifeUndoState: Equatable, Sendable {
+    let restoreValue: Int
+    let operationID: UUID
 }
 
 enum LocalCommanderTaxSlot: Equatable, Sendable {
